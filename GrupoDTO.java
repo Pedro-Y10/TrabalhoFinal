30fbd67c6efc7e279972684268d876aa224c3536
Pedro-Y10 <152759796+Pedro-Y10@users.noreply.github.com>
2025-01-25 09:13:39 -0300

    Add files via upload

---
+++ GrupoDTO.java
@@ -0,0 +1,13 @@
+package br.edu.iffarroupilha.sigachat.modelos.dto;
+
+import br.edu.iffarroupilha.sigachat.modelos.Usuario;
+/**
+ * <p>
+ * Objeto de transferencia de dados para a entidade grupo
+* </p>
+* @author Professor
+* @since Nov 6, 2024 7:34:05 PM
+*/
+public record GrupoDTO (long id, String email, String nome) {
+
+}
